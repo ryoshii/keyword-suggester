b4028adf4ef162d7654f47f61f2780e1ef22e1bf
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-07-06 12:00:04 -0300

    feat: Add show keywords

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useState, useCallback, SyntheticEvent } from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  Form,
+  Input,
+  SearchButton,
+  Result,
+  Item,
+  ShowTextareaButton,
+  ResultText,
+} from './styles';
 
 const Home: React.FC = () => {
-  return <Container>Home</Container>;
+  const [showTextarea, setShowTextarea] = useState(false);
+  const [keywords, setKeywords] = useState<string[]>([]);
+  const [keywordsText, setKeywordsText] = useState<string>('');
+
+  const handleToogleShowTextarea = useCallback(() => {
+    if (!showTextarea) {
+      const compiledWordsToText = keywords.join(',');
+      setKeywordsText(compiledWordsToText);
+    } else {
+      const compiledWordsToArray = keywordsText.split(',');
+      setKeywords([...compiledWordsToArray]);
+    }
+    setShowTextarea(!showTextarea);
+  }, [showTextarea, keywords, keywordsText]);
+
+  const handleSubmit = useCallback((event: SyntheticEvent) => {
+    event.preventDefault();
+    const fakeWords = [
+      'Palavra 1',
+      'Palavra 2',
+      'Palavra 3',
+      'Palavra 4',
+      'Palavra 5',
+      'Palavra 7',
+    ];
+
+    setKeywords([...fakeWords]);
+  }, []);
+
+  const handleTextareaChange = useCallback(
+    (event: React.FormEvent<HTMLTextAreaElement>) => {
+      setKeywordsText(event.currentTarget.value);
+    },
+    [],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <Title># Keyword suggester</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input name="param" placeholder="subject" />
+          <SearchButton type="submit">Search</SearchButton>
+        </Form>
+        {!showTextarea && (
+          <Result>
+            {keywords &&
+              keywords.map((word, index) => <Item key={index}>{word}</Item>)}
+          </Result>
+        )}
+
+        {showTextarea && (
+          <ResultText
+            name="description"
+            onChange={handleTextareaChange}
+            defaultValue={keywordsText}
+          />
+        )}
+
+        <ShowTextareaButton onClick={handleToogleShowTextarea}>
+          {!showTextarea && 'Edit mode'}
+          {showTextarea && 'Box mode'}
+        </ShowTextareaButton>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
--- web/src/pages/Home/styles.ts
+++ web/src/pages/Home/styles.ts
@@ -1,3 +1,97 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+
+  border: 1px solid red;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+
+  padding: 24px;
+`;
+
+export const Title = styled.h1`
+  margin: 8px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Input = styled.input`
+  padding: 16px;
+  margin-right: 8px;
+  border: 1px solid #aaaaaa;
+
+  &:focus {
+    padding: 15px;
+    border: 2px solid #111111;
+  }
+`;
+
+export const SearchButton = styled.button`
+  padding: 16px;
+  border: 1px solid #aaaaaa;
+  background: #dddddd;
+
+  &:hover {
+    padding: 15px;
+    border: 2px solid #111111;
+  }
+`;
+
+export const Result = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 2px solid #111111;
+  height: 400px;
+  width: 100%;
+  margin-top: 8px;
+  padding: 8px;
+  align-items: start;
+  align-content: start;
+`;
+
+export const Item = styled.div`
+  background: #ff4136;
+  padding: 8px 16px 8px 16px;
+  margin-top: 4px;
+  margin-left: 4px;
+`;
+
+export const ShowTextareaButton = styled.button`
+  margin-top: 24px;
+  padding: 8px;
+  border: 1px solid #aaaaaa;
+  background: #dddddd;
+
+  &:hover {
+    padding: 7px;
+    border: 2px solid #111111;
+  }
+`;
+
+export const ResultText = styled.textarea`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 2px solid #111111;
+  height: 400px;
+  width: 100%;
+  margin-top: 8px;
+  padding: 8px;
+  align-items: start;
+  align-content: start;
+`;
