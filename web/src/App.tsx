b9c793f097223258e60eb84590620061598da800
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-07-06 08:54:45 -0300

    feat: Add routes and initial struture folders

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => (
-  <>
-    <h1>TESTE</h1>
+  <Router>
+    <Routes />
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
